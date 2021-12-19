63b7865760703081de5f35148912cce8e55a4a83
JSchrtke <jschrtke@protonmail.com>
2021-12-19 21:08:27 +0100

    Add day 1, part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,13 +1,25 @@
 pub fn run(input: &str) -> String {
-    part1(input)
+    format!(
+        "depth changed {} individual times and {} times on average",
+        part1(input),
+        part2(input)
+    )
 }
 
-fn part1(input: &str) -> String {
-    let nums = input
+fn part1(input: &str) -> i32 {
+    let nums = nums_from_input(input);
+
+    count_increments(nums)
+}
+
+fn nums_from_input(input: &str) -> Vec<i32> {
+    input
         .split("\n")
         .map(|x| x.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
+        .collect::<Vec<i32>>()
+}
 
+fn count_increments(nums: Vec<i32>) -> i32 {
     let mut inc_cnt = 0;
     let mut prev: i32 = *nums.first().unwrap();
     for n in nums {
@@ -17,5 +29,28 @@ fn part1(input: &str) -> String {
         prev = n;
     }
 
-    format!("depth changed {} times", inc_cnt)
+    inc_cnt
+}
+
+fn part2(input: &str) -> i32 {
+    let nums = nums_from_input(input);
+    let averages = sliding_average(nums, 3);
+
+    count_increments(averages)
+}
+
+fn sliding_average(values: Vec<i32>, win_size: usize) -> Vec<i32> {
+    if win_size >= values.len() {
+        return values;
+    }
+
+    let mut avgs = Vec::new();
+    for i in 0..values.len() - 2 {
+        for _j in 0..win_size {
+            let a = values[i] + values[i + 1] + values[i + 2];
+            avgs.push(a as i32)
+        }
+    }
+
+    avgs
 }
