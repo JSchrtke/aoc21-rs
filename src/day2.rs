5b359af7ab9d842e79ffb3259a2b3f0221e1ee55
js <jschrtke@protonmail.com>
2021-12-24 18:24:34 +0100

    Add day 2, part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,5 +1,5 @@
 pub fn run(input: &str) -> String {
-    part1(input)
+    part2(input)
 }
 
 fn part1(input: &str) -> String {
@@ -14,6 +14,29 @@ fn part1(input: &str) -> String {
             "forward" => x_pos += n,
             "up" => y_pos -= n,
             "down" => y_pos += n,
+            _ => panic!("Invalid direction"),
+        }
+    });
+
+    format!("[day 2] Final location is {}", x_pos * y_pos)
+}
+
+fn part2(input: &str) -> String {
+    let mut x_pos = 0;
+    let mut y_pos = 0;
+    let mut aim = 0;
+
+    input.split("\n").for_each(|s| {
+        let mut it = s.split(" ");
+        let s = it.next().unwrap();
+        let n = it.next().unwrap().parse::<i32>().unwrap();
+        match s {
+            "down" => aim += n,
+            "up" => aim -= n,
+            "forward" => {
+                x_pos += n;
+                y_pos += n * aim;
+            }
             _ => panic!("Invalid direction"),
         }
     });
