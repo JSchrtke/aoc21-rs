6c5e13a4889172d2a23d99a49df63ba1802e2c57
js <jschrtke@protonmail.com>
2021-12-28 17:06:49 +0100

    Refactor day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,27 +1,34 @@
 pub fn run(input: &str) -> String {
+    let depths = input.split("\n").map(|s| s.parse().unwrap()).collect();
+
+    let d = count_increments(&depths);
+    assert_eq!(1681, d);
+
+    let a = average_depth_increases(&depths);
+    assert_eq!(1704, a);
+
     format!(
         "Depth changed {} individual times and {} times on average",
-        part1(input),
-        part2(input)
+        d, a,
     )
 }
 
-fn part1(input: &str) -> i32 {
-    let numbers = numbers_from_input(input);
+fn average_depth_increases(numbers: &Vec<i32>) -> i32 {
+    let mut averages = Vec::new();
+    for i in 0..numbers.len() - 2 {
+        let mut average = 0;
+        for j in 0..3 {
+            average += numbers[i + j];
+        }
+        averages.push(average);
+    }
 
-    count_increments(numbers)
+    count_increments(&averages)
 }
 
-fn numbers_from_input(input: &str) -> Vec<i32> {
-    input
-        .split("\n")
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>()
-}
-
-fn count_increments(numbers: Vec<i32>) -> i32 {
+fn count_increments(numbers: &Vec<i32>) -> i32 {
     let mut increment_count = 0;
-    let mut previous_n: i32 = *numbers.first().unwrap();
+    let mut previous_n = numbers.first().unwrap();
     for n in numbers {
         if n > previous_n {
             increment_count += 1;
@@ -29,29 +36,5 @@ fn count_increments(numbers: Vec<i32>) -> i32 {
         previous_n = n;
     }
 
-    increment_count
-}
-
-fn part2(input: &str) -> i32 {
-    let numbers = numbers_from_input(input);
-    let averages = sliding_average(numbers, 3);
-
-    count_increments(averages)
-}
-
-fn sliding_average(values: Vec<i32>, window_size: usize) -> Vec<i32> {
-    if window_size >= values.len() {
-        return values;
-    }
-
-    let mut averages = Vec::new();
-    for i in 0..values.len() - 2 {
-        let mut average: i32 = 0;
-        for j in 0..window_size {
-            average += values[i + j];
-        }
-        averages.push(average as i32);
-    }
-
-    averages
+    return increment_count;
 }
