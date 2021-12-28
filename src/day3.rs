fdc7eed7eeaeee350113f1557b22193fb5b11f70
js <jschrtke@protonmail.com>
2021-12-28 15:26:35 +0100

    Finish day 3, part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,20 +1,28 @@
 pub fn run(input: &str) -> String {
-    let pc = part1(input);
-    let x = part2(input);
-    format!("power consumption is {}\n\t{}", pc, x)
+    let num_strs: Vec<&str> = input.split("\n").collect();
+
+    let pc = part1(&num_strs);
+    assert_eq!(4139586, pc);
+
+    let l = part2(&num_strs);
+    assert_eq!(1800151, l);
+
+    format!(
+        "power consumption is {}\n\tlife support rating is {}",
+        pc, l
+    )
 }
 
-fn part1(input: &str) -> i32 {
-    let num_list: Vec<&str> = input.split("\n").collect();
-    let num_cnt = num_list.len();
-    let num_len = num_list.first().unwrap().len();
+fn part1(num_strs: &Vec<&str>) -> i32 {
+    let num_cnt = num_strs.len();
+    let bit_cnt = num_strs.first().unwrap().len();
 
-    let mut counts = vec![0; num_len];
+    let mut high_cnts = vec![0; bit_cnt];
 
-    for num in num_list {
+    for num in num_strs {
         for (index, c) in num.chars().enumerate() {
             match c {
-                '1' => counts[index] += 1,
+                '1' => high_cnts[index] += 1,
                 '0' => continue,
                 _ => panic!("something"),
             }
@@ -22,7 +30,7 @@ fn part1(input: &str) -> i32 {
     }
 
     let mut gamma_rate: i32 = 0;
-    for n in counts {
+    for n in high_cnts {
         gamma_rate = gamma_rate << 1;
         if n > num_cnt / 2 {
             gamma_rate += 1;
@@ -33,19 +41,19 @@ fn part1(input: &str) -> i32 {
         }
     }
 
-    let epsilon_mask = 2i32.pow(num_len as u32) - 1;
+    let epsilon_mask = 2i32.pow(bit_cnt as u32) - 1;
     let epsilon_rate = !gamma_rate & epsilon_mask;
 
-    gamma_rate * epsilon_rate
+    let pwr_consumption = gamma_rate * epsilon_rate;
+
+    return pwr_consumption;
 }
 
-fn part2(input: &str) -> String {
-    let o2 = o2_gen_rating(input);
-    format!("o2 rating is {}", o2)
+fn part2(num_strs: &Vec<&str>) -> u32 {
+    o2_gen_rating(num_strs) * co2_scrubber_rating(num_strs)
 }
 
-fn o2_gen_rating(input: &str) -> u32 {
-    let num_strs: Vec<&str> = input.split("\n").collect();
+fn o2_gen_rating(num_strs: &Vec<&str>) -> u32 {
     let bit_cnt = num_strs.first().unwrap().len();
 
     let mut nums: Vec<u32> = num_strs
@@ -69,10 +77,36 @@ fn o2_gen_rating(input: &str) -> u32 {
 
         nums = nums.into_iter().filter(|n| (n & mask) == filter).collect();
 
-        mask = mask >> 1;
+        mask >>= 1;
     }
 
-    let res = nums.first().unwrap();
+    let o2 = nums.first().unwrap();
 
-    *res
+    return *o2;
+}
+
+fn co2_scrubber_rating(num_strs: &Vec<&str>) -> u32 {
+    let mut nums: Vec<u32> = num_strs
+        .iter()
+        .map(|s| u32::from_str_radix(s, 2).expect("not a valid binary number"))
+        .collect();
+
+    let bit_cnt = num_strs.first().unwrap().len() as u32;
+    let mut mask = 2u32.pow(bit_cnt - 1);
+
+    while nums.len() > 1 {
+        let high_cnts = nums.iter().filter(|&n| n & mask == mask).count();
+
+        let mut filter = mask;
+        if high_cnts * 2 >= nums.len() {
+            filter = 0;
+        }
+
+        nums = nums.into_iter().filter(|&n| n & mask == filter).collect();
+
+        mask >>= 1;
+    }
+    let co2 = *nums.first().unwrap();
+
+    co2
 }
